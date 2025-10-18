7c2a979e5578bfd2b5db7428a5be52febfca3ff8
Granit <granit.g4shii@gmail.com>
2025-10-18 17:50:20 +0200

    Revert "seperate landing page from dashboard (refs #11)"
    This reverts commit e846cf5fb0b956a5b59202f16d0b644ea9dda30e.

--- next.config.js
+++ next.config.js
@@ -4,20 +4,6 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com"],
   },
-  // Allow both domains
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
